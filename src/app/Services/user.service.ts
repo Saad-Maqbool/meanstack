cdfc0c3a68c1afd43e8ff6f48e2a2e2d7b13085b
Saad-Maqbool <k122187@nu.edu.pk>
2018-05-23 17:55:10 +0500

    cors issue

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,38 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {tokenKey} from '@angular/core/src/view';
 import {Token} from '@angular/compiler';
 
 @Injectable()
 export class UserService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
 
   signup(user) {
-    const headerDict = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    };
 
-    const requestOptions = {
-      headers: new HttpHeaders(headerDict),
-    };
-
-    return this.http.post('/users/register', user, requestOptions);
+    return this.http.post('/users/register', user);
   }
+
   signin(user) {
-    const headerDict = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    };
-
-    const requestOptions = {
-      headers: new HttpHeaders(headerDict),
-    };
-
-    const req = this.http.post('/users/login', user, requestOptions)
+    const req = this.http.post('/users/login', user)
       .subscribe(
         res => {
           console.log(res);
